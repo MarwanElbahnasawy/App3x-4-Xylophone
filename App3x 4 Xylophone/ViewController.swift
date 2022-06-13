431aed4112c795e6ce370dd94816b4783f2b6058
Marwan <marwan.elbahnasawy@gmail.com>
2022-06-13 21:53:13 +0200

    Initial Commit

---
+++ App3x 4 Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  App3x 4 Xylophone
+//
+//  Created by Marwan Elbahnasawy on 21/05/2022.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        play(sound: (sender.titleLabel!.text!))
+    }
+    
+    func play(sound: String){
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+}
+
